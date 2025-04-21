574ca20b62d7d16d67c4170b7d2e110c7aecb7e3
Carlos Verdes <cverdes@gmail.com>
2025-04-21 14:52:41 +0400

    chore: config vite to build a library

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { dirname, format, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/lib/components/index.js'),
+      name: 'Funkode UI',
+      // the proper extensions will be added
+      fileName: 'funkode-ui',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
