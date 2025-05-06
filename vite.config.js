2c5ed672b4ec03fc5c8e2ce6d90613a6a30d12a0
Carlos Verdes <cverdes@gmail.com>
2025-05-06 08:19:22 +0400

    chore: small changes in config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath } from 'url';
 export default defineConfig({
   plugins: [dts({ include: ['lib']})],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es']
