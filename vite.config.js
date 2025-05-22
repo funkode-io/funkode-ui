b5c04641b6bf698fa04a2ab5763d99f560647d0d
Carlos Verdes <cverdes@gmail.com>
2025-05-22 17:41:13 +0400

    feat: add css to bundle and vite test app

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,21 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import tailwindcss from "@tailwindcss/vite";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 import { glob } from "glob";
 
 export default defineConfig({
-  plugins: [dts({ include: ["lib"] }), tailwindcss()],
+  plugins: [libInjectCss(), dts({ include: ["lib"] }), tailwindcss()],
   resolve: {
     alias: {
-      "@/ui": resolve(__dirname, "./lib/ui"),
+      "@/ui": resolve(__dirname, "./lib/components/ui"),
       "@/public": resolve(__dirname, "./public"),
     },
   },
   build: {
     copyPublicDir: false,
+    cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
@@ -36,6 +38,8 @@ export default defineConfig({
       output: {
         assetFileNames: "assets/[name][extname]",
         entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name].[hash].js",
+        preserveModules: false,
       },
     },
   },
