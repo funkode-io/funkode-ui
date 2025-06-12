9657420d963c271d772c0933fc10362c373d850b
Carlos Verdes <cverdes@gmail.com>
2025-06-12 12:38:58 +0400

    chore: integration with htmx

--- src/stories/demo.stories.ts
+++ src/stories/demo.stories.ts
@@ -62,7 +62,7 @@ export const DemoPage: Story = {
     msw: {
       handlers: [
         http.get("https://funkode.io/funkode-ui/auth/challenge", () => HttpResponse.text(CHALLENGE)),
-        http.get("/home", () => HttpResponse.html("<p>hello Peter</p>")),
+        http.get("/home", () => HttpResponse.html(demoChartHtml)),
       ],
     },
   },
@@ -206,117 +206,118 @@ export const DemoPage: Story = {
     `,
 };
 
-const demoChartHtml = html`
+const demoChartHtml = `
   <div id="chartContainer">
-    <div id="mainChart" class="h-96 w-full">
+    <div id="mainChart" class="h-96 w-full bg-base-300 border border-base-100">
     </div>
   </div>
   <script type="text/javascript">
+  console.log("Loading ECharts demo chart...");
+  htmx.onLoad(function() {
+    console.log("htmx loaded, initializing ECharts...");
   
-  document.addEventListener('DOMContentLoaded', function() {
-  const myChartElement = document.getElementById('mainChart');
-  var myChart = echarts.init(myChartElement);
+    const myChartElement = document.getElementById('mainChart');
+    var myChart = echarts.init(myChartElement);
 
-  const styles = getComputedStyle(myChartElement);
-  const backgroundColor = styles.getPropertyValue('--color-base-300') || '#ffffff';
-  const baseContentColor = styles.getPropertyValue('--color-base-content') || '#000000';
-  const accentColor = styles.getPropertyValue('--color-accent') || '#ff5722';
-  const accentColorContent = styles.getPropertyValue('--color-accent-content') || '#ffffff';
-  const infoColor = styles.getPropertyValue('--color-info') || '#2196f3';
-  const infoColorContent = styles.getPropertyValue('--color-info-content') || '#ffffff';
-  const successColor = styles.getPropertyValue('--color-success') || '#4caf50';
-  const warningColor = styles.getPropertyValue('--color-warning') || '#ff9800';
-  const errorColor = styles.getPropertyValue('--color-error') || '#f44336';
-  const neutralColor = styles.getPropertyValue('--color-neutral') || '#9e9e9e';
-  const neutralContentColor = styles.getPropertyValue('--color-neutral-content') || '#212121';
+    const styles = getComputedStyle(myChartElement);
+    const backgroundColor = styles.getPropertyValue('--color-base-300') || '#ffffff';
+    const baseContentColor = styles.getPropertyValue('--color-base-content') || '#000000';
+    const accentColor = styles.getPropertyValue('--color-accent') || '#ff5722';
+    const accentColorContent = styles.getPropertyValue('--color-accent-content') || '#ffffff';
+    const infoColor = styles.getPropertyValue('--color-info') || '#2196f3';
+    const infoColorContent = styles.getPropertyValue('--color-info-content') || '#ffffff';
+    const successColor = styles.getPropertyValue('--color-success') || '#4caf50';
+    const warningColor = styles.getPropertyValue('--color-warning') || '#ff9800';
+    const errorColor = styles.getPropertyValue('--color-error') || '#f44336';
+    const neutralColor = styles.getPropertyValue('--color-neutral') || '#9e9e9e';
+    const neutralContentColor = styles.getPropertyValue('--color-neutral-content') || '#212121';
 
-  // Each item: open，close，lowest，highest
-const data0 = splitData([
-  ['2013/1/24', 2320.26, 2320.26, 2287.3, 2362.94],
-  ['2013/1/25', 2300, 2291.3, 2288.26, 2308.38],
-  ['2013/1/28', 2295.35, 2346.5, 2295.35, 2346.92],
-  ['2013/1/29', 2347.22, 2358.98, 2337.35, 2363.8],
-  ['2013/1/30', 2360.75, 2382.48, 2347.89, 2383.76],
-  ['2013/1/31', 2383.43, 2385.42, 2371.23, 2391.82],
-  ['2013/2/1', 2377.41, 2419.02, 2369.57, 2421.15],
-  ['2013/2/4', 2425.92, 2428.15, 2417.58, 2440.38],
-  ['2013/2/5', 2411, 2433.13, 2403.3, 2437.42]
-]);
+    // Each item: open,close,lowest,highest
+    const data0 = splitData([
+      ['2013/1/24', 2320.26, 2320.26, 2287.3, 2362.94],
+      ['2013/1/25', 2300, 2291.3, 2288.26, 2308.38],
+      ['2013/1/28', 2295.35, 2346.5, 2295.35, 2346.92],
+      ['2013/1/29', 2347.22, 2358.98, 2337.35, 2363.8],
+      ['2013/1/30', 2360.75, 2382.48, 2347.89, 2383.76],
+      ['2013/1/31', 2383.43, 2385.42, 2371.23, 2391.82],
+      ['2013/2/1', 2377.41, 2419.02, 2369.57, 2421.15],
+      ['2013/2/4', 2425.92, 2428.15, 2417.58, 2440.38],
+      ['2013/2/5', 2411, 2433.13, 2403.3, 2437.42]
+    ]);
 
-  var option;
+    var option;
 
-  option = {
-    color: [
-        "#893448",
-        "#d95850",
-        "#eb8146",
-        "#ffb248",
-        "#f2d643",
-        "#ebdba4"
-    ],
-    backgroundColor: backgroundColor,
-    textStyle: {},
-    title: {
-      text: "ECharts Demo Chart",
-      subtext: "A simple candlestick chart",
-      left: "center",
-      textStyle: {
-          color: baseContentColor
+    option = {
+      color: [
+          "#893448",
+          "#d95850",
+          "#eb8146",
+          "#ffb248",
+          "#f2d643",
+          "#ebdba4"
+      ],
+      backgroundColor: backgroundColor,
+      textStyle: {},
+      title: {
+        text: "ECharts Demo Chart",
+        subtext: "A simple candlestick chart",
+        left: "center",
+        textStyle: {
+            color: baseContentColor
+        },
+        subtextStyle: {
+            color: infoColor
+        }
       },
-      subtextStyle: {
-          color: infoColor
-      }
-    },
-    xAxis: {
-      type: 'category',
-      data: data0.categoryData,
-      boundaryGap: false,
-      axisLine: { onZero: false },
-      splitLine: { show: false },
-      min: 'dataMin',
-      max: 'dataMax'
-    },
-    yAxis: {
-      scale: true,
-      splitArea: {
-        show: true
-      }
-    },
-    series: [
-      {
-        type: 'candlestick',
-        itemStyle: {
-             color: accentColor,       // Color for bullish candles
-             color0: neutralColor,        // Color for bearish candles
-             borderColor: accentColorContent, // Border color for all candles
-             borderColor0: neutralContentColor,
-             borderWidth: '2' // Border color for all candles
-         },
-        data: data0.values,
+      xAxis: {
+        type: 'category',
+        data: data0.categoryData,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        splitLine: { show: false },
+        min: 'dataMin',
+        max: 'dataMax'
       },
-      
-    ]
-  };
+      yAxis: {
+        scale: true,
+        splitArea: {
+          show: true
+        }
+      },
+      series: [
+        {
+          type: 'candlestick',
+          itemStyle: {
+               color: accentColor,       // Color for bullish candles
+               color0: neutralColor,        // Color for bearish candles
+               borderColor: accentColorContent, // Border color for all candles
+               borderColor0: neutralContentColor,
+               borderWidth: '2' // Border color for all candles
+           },
+          data: data0.values,
+        },
 
-  option && myChart.setOption(option);
+      ]
+    };
+
+    option && myChart.setOption(option);
   });
-  function splitData(rawData) {
+  
+  function splitData(rawData) {  
     const categoryData = [];
     const values = [];
     for (var i = 0; i < rawData.length; i++) {
       categoryData.push(rawData[i].splice(0, 1)[0]);
       values.push(rawData[i]);
     }
+    
     return {
       categoryData: categoryData,
       values: values
     };
-}
+  }
 </script>
 `;
-
-const template = document.createElement("template");
-const demoChartRendered = render(demoChartHtml, template);
 
 export const DemoChart: Story = {
   render: () => demoChartHtml,
